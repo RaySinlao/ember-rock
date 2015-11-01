fded2aa70ee7bb66b0e419af96459d53833645be
Raymund Sinlao <raysinlao@Raymunds-MacBook-Pro.local>
2015-11-01 14:20:55 -0800

    Chapter 6

--- app/models/band.js
+++ app/models/band.js
@@ -4,13 +4,23 @@ import DS from 'ember-data';
 export default Ember.Object.extend({
   name: '',
 
+  /*
+   * If you don't explicitly pass in an empty songs array for each band object you create, 
+   * they will share the underlying array defined on the class (the prototype).
+   *
+   */
+
   setupSongs: Ember.on('init', function() {
+
+    // Defines songs array for new band
     if (!this.get('songs')) {
       this.set('songs', []);
     }
   }),
 
+  // Creates a slug for the URL
   slug: Ember.computed('name', function() {
     return this.get('name').dasherize();
-  }),
+  })
+
 });
